af347aa59f9c4257c507d7f3657bde5a9944d13a
Martin Thomsen <201800305@post.au.dk>
2020-02-05 15:15:47 +0100

    Tilføjelse af testproject

---
+++ Jenkins/NUnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    
+    public class UnitTest1
+    {
+        [TestCase()]
+        public void TestMethod1()
+        {
+        }
+    }
+}
