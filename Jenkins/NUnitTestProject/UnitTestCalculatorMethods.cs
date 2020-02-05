3bea7f07494d41195748917d5d45e42ffb062afb
Martin Thomsen <201800305@post.au.dk>
2020-02-05 15:42:59 +0100

    Fjernet tekst

--- Jenkins/NUnitTestProject/UnitTestCalculatorMethods.cs
+++ Jenkins/NUnitTestProject/UnitTestCalculatorMethods.cs
@@ -21,9 +21,6 @@ namespace NUnitTestProject
             public void UnitTestAdd(double a, double b, double result)
             {
                 Assert.AreEqual(uut.Add(a, b), result, 0.0001);
-                // AreEqual er en classic assert model, hvorimod That.(uut.Add(a,b) == result) er constraint-based model
-                // Assert.That(uut.Add(a, b), Is.EqualTo(result).Within(0.0001));
-                // Løst med en constraint-based model
             }
 
             [TestCase(3.75, 4.26, -0.51)]
@@ -134,7 +131,6 @@ namespace NUnitTestProject
             {
                 uut.Power(a, exp);
 
-                //Denne metode er testet ved at finde hvad resultatet er vha. en lommeregner...
                 Assert.AreEqual(uut.PowerToAccumulator(exp), 1529326745.2420878, 0.0001);
             }
 
@@ -146,35 +142,5 @@ namespace NUnitTestProject
                 Assert.AreEqual(uut.DivideAccumulatorWith(a), result / a, 0.0001);
             }
 
-
-
-
-
-            //HVAD ER DETTE??
-
-            //[TestCase(1, 2, 3)]
-            //public void UnitTestAccumulator_Add(double a, double b, double result)
-            //{
-            //    uut.Add(a, b);
-            //    Assert.AreEqual((uut.Accumulator), result, 0.0001);
-            //}
-
-            //[TestCase(1, 2, 3)]
-            //public void UnitTestAccumulator_Add_Twice(double a, double b, double result)
-            //{
-            //    uut.Add(100, 100);
-            //    uut.Add(a, b);
-            //    Assert.AreEqual((uut.Accumulator), result, 0.0001);
-            //}
-
-            //[TestCase(1, 2, 3, 100, 100)]
-            //[TestCase(1, 2, 3, 0, 100)]
-            //[TestCase(1, 2, 3, 100, 0)]
-            //public void UnitTestAccumulator_Add_DivideFirst(double a, double b, double result, double dividend, double divisor)
-            //{
-            //    uut.Divide(dividend, divisor);
-            //    uut.Add(a, b);
-            //    Assert.AreEqual((uut.Accumulator), result, 0.0001);
-            //}
         }
     }
